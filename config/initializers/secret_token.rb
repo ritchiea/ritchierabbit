29303749a730761d05ed77ca565a1038f265bd04
Andrew Ritchie <ritchiea@gmail.com>
2014-02-21 16:38:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ritchierabbit::Application.config.secret_key_base = '50606c613aaadb6d2c6d907477ceb1c476957f4d336cf0d67775970787f237792f567590e2f7c3af008ca0a7894dcabb6a7273ff4da13e88d667c24d4a18098c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ritchierabbit::Application.config.session_store :cookie_store, key: '_ritchierabbit_session'
